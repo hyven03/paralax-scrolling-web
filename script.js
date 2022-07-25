3971a06d18746be1ce863b06fab41f6e0ecd06ca
hyven03 <huyvb250103@gmail.com>
2022-07-25 23:25:02 +0700

    test

---
+++ script.js
@@ -0,0 +1,18 @@
+let stars = document.getElementById("stars");
+let moon = document.getElementById("moon");
+let mountains_behind = document.getElementById("mountains_behind");
+let Fullname = document.getElementById("Fullname");
+let Info = document.getElementById("Info");
+let mountains_front = document.getElementById("mountains_front");
+let header = document.querySelector("header");
+
+window.addEventListener("scroll", function () {
+  let value = window.scrollY;
+  stars.style.left = value * 0.25 + "px";
+  moon.style.top = value * 1 + "px";
+  mountains_behind.style.top = value * 0.5 + "px";
+  mountains_front.style.top = value * 0 + "px";
+  Fullname.style.marginRight = value * 4 + "px";
+  Fullname.style.marginTop = value * 1 + "px";
+  header.style.marginTop = value * 0.2 + "px";
+});
